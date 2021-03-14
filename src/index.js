6cbebd8a1ce0558f8861bd3ad39db7dd5c5dee25
ivanov-in <work.ivanov.in@gmail.com>
2021-03-14 16:14:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+      n *= -1;
+  }
+
+  let outputArray = n.toString().split('');
+  outputArray.reverse();
+  n = outputArray.join('');
+  return n;
 }
